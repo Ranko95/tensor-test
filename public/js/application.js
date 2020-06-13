140b16b40934a181381cddc28e32215a6b96e591
Ranko95 <ranko.stevanovich@gmail.com>
2020-06-13 22:27:55 +0300

    chat layout, styles, sockets

---
+++ public/js/application.js
@@ -0,0 +1,7 @@
+const sendForm = document.querySelector('.input-form');
+const textArea = document.querySelector('.text-area');
+
+sendForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  document.createElement('div');
+});
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 /* eslint-disable no-restricted-syntax */
 const http = require('http');
-const moment = require('moment');
+const socketio = require('socket.io');
 const app = require('./app');
 
 const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
+
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  })
+});
 
 server.listen(port, () => console.log(`Listening on ${port}`));
